ab6f272b15b1458556f5d057aabb83f3605472e0
FranciscoVigo <79640958+FranciscoVigo@users.noreply.github.com>
2023-04-24 12:09:03 +0200

    c3

--- src/ll.java
+++ src/ll.java
@@ -1,10 +1,17 @@
 public class ll {
     int a;
+    int b;
     public ll() {
         this.a = 0;
     }
     public ll(int a) {
         this.a = a;
     }
+
+    public ll(int a, int b){
+        this.a=a;
+        this.b=b;
+    }
+
 
 }
